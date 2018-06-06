ac515384730c7812e4189297c1419bfbd5545052
Lorena <lorena.giddens@outlook.com>
2018-06-06 01:01:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word(string)
   words = string.split(" ")
   # loop through each word in a string & add reversed word to a new array
   new_string = []
-  words.each {|word| new_string << words.reverse}
+  words.each {|word| new_string << words.reverse!}
   # convert new array of reversed words into string by joining elements by space
   new_string.join(" ")
 end
@@ -15,6 +15,6 @@ end
 def reverse_each_word(string)
   words = string.split
   new_string = []
-  words.collect {|word| new_string << words.reverse}
+  words.collect {|word| new_string << words.reverse!}
   new_string.join(" ")
 end
